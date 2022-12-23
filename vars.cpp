fec05b9a5ec597301721a764d6c1d4890af520c1
Annaliese B <84485729+ZehavaBatya@users.noreply.github.com>
2022-12-23 10:19:56 -0500

    Create vars.cpp

---
+++ vars.cpp
@@ -0,0 +1,22 @@
+// A variable declaration has this syntax: data-type variable-name;
+// Multiple variablesx of the same data type in a single declaration: data-type variable-name1, variable-name2, variable-name3;
+
+// 5 basic data types: 1. char , 2. int, 3. float, 4. double, 5. bool
+// char - a single byte, capable of holding one character. Example: 'A' 
+// int - an integer whole number. Example: 100
+// float - a floating-point number, correct to 6 decimal places. Example: 0.123456
+// double - a floaitng-point number, correct to 10 decimal places. Example: 0.0123456789
+// bool - a Boolean vlaye of true or false, or numerically zero is false, and any non-zero is true. Example: false or 0. true or 1
+
+#inclide <iostream>
+using namespacce std;
+int main()
+{
+  // Program goes here
+  char letter: A
+  int number: 100
+  float decimal: 7.5
+  double pi: 3.14159
+  bool isTrue: 0
+  cout << "char letter" << letter << endl;
+}
